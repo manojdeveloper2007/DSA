b7d1f4699fc6f6e00a20f168390f246e2981834e
manojdeveloper2007 <manoj.developer2007@gmail.com>
2026-08-14 10:08:41 +0530

    Leetcode (Peak Element)

---
+++ BinarySearch/peakElement.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int findPeakElement(vector<int> &nums)
+{
+    int n = nums.size();
+
+    if (nums.size() == 1)
+    {
+        return 0;
+    }
+
+    if (nums[0] > nums[1])
+    {
+        return 0;
+    }
+
+    if (nums[n - 1] > nums[n - 2])
+    {
+        return n - 1;
+    }
+
+    int low = 1;
+    int high = n - 2;
+
+    while (low <= high)
+    {
+        int mid = (low + high) / 2;
+
+        if (nums[mid] > nums[mid - 1] && nums[mid] > nums[mid + 1])
+        {
+            return mid;
+        }
+
+        else if (nums[mid] < nums[mid + 1])
+        {
+            low = mid + 1;
+        }
+        else
+        {
+            high = mid - 1;
+        }
+    }
+    return -1;
+}
+
+int main() {
+    vector<int> nums = {1,2,3,4,0};
+    cout<<findPeakElement(nums)<<endl;
+}
